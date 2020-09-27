10dbea9e8973f59ef370ffb45b945ad920fcd16f
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-26 21:40:14 -0400

    start and next button, start game function

---
+++ script.js
@@ -0,0 +1,24 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const timer = document.getElementById("timer")
+const score = document.getElementById("score")
+const initials = document.getElementById("initials")
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+let shuffledQuestions, currentQuestionIndex
